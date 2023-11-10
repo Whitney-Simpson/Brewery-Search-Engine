71a34b67e9e39eb26bcfb149a43d5ebddf54513f
Goobergreve09 <gregory.greve@yahoo.com>
2023-11-09 19:37:35 -0500

    weather API foundation functionality pushed

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const theDate = document.getElementById("currentDate");
 
-    // Assuming you have the rest of your code here.
+
+
 
     const searchButton = document.getElementById("btn");
-    const locationInput = document.getElementById("location");
+    const locationInput = document.getElementById("location"); //Input for the users location text
 
     searchButton.addEventListener("click", function(event) {
         event.preventDefault();
         var apiKey = "a1c24f9ef9bb705299a22d8524be3474";
-        const location = locationInput.value;
+        const location = locationInput.value; //Turns the city location typed by the user into a variable
 
         // Make the API request to OpenWeatherMap's Geocoding API
         const geocodingUrl = `https://api.openweathermap.org/geo/1.0/direct?q='${location}'&limit=50&appid=${apiKey}`;
@@ -33,3 +33,5 @@ document.addEventListener("DOMContentLoaded", function() {
        
     });
 });
+
+//Line 1-35 Weather API functionality
