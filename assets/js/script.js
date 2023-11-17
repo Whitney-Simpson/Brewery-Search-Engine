9976dbe8e4edd858c46bb24db5684a8d098a60f3
Goobergreve09 <gregory.greve@yahoo.com>
2023-11-16 19:40:51 -0500

    map and weather section stylized

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,6 +47,7 @@ document.addEventListener("DOMContentLoaded", function () {
   });
   locationInput.addEventListener("keyup", function (event) {
     if (event.key === "Enter") {
+      event.preventDefault();
       searchButton.click();
     }
   });
@@ -67,6 +68,7 @@ function weatherWidget(
   h2El.style.color = "white";
   h2El.style.textAlign = "center";
   h2El.style.fontSize = "40px";
+  h2El.style.fontWeight = "bold";
 
   const h3El = document.createElement("h3");
   h3El.textContent = "Current Weather Conditions in " + cityName;
@@ -75,6 +77,7 @@ function weatherWidget(
   h3El.style.textAlign = "center";
   h3El.style.fontSize = "30px";
   h3El.style.width = "500px";
+  h3El.style.fontWeight = "bold";
 
   const ulEl = document.createElement("ul");
   ulEl.style.textAlign = "center";
@@ -83,10 +86,14 @@ function weatherWidget(
   ulEl.style.borderRadius = "25px";
   ulEl.style.marginLeft = "auto";
   ulEl.style.marginRight = "auto";
+  ulEl.style.backgroundSize = "cover";
+  ulEl.style.backgroundRepeat = "no-repeat";
+  ulEl.style.backgroundImage =
+    'url("assets/Images/weather-images/woodBackground.jpg")';
 
   const pEl = document.createElement("p");
   pEl.style.color = "white";
-  pEl.style.backgroundColor = "rgba(0, 0, 0, 0.4)";
+  pEl.style.backgroundColor = "rgba(0, 0, 0, 0.6)";
   pEl.style.borderRadius = "25px";
   pEl.style.width = "475px";
   pEl.style.padding = "10px";
@@ -97,8 +104,12 @@ function weatherWidget(
   pEl.style.paddingBottom = "0";
   pEl.style.fontFamily = "Raleway, sans-serif";
   pEl.style.fontSize = "18px";
+  pEl.style.fontWeight = "bold";
 
-  const mapWeatherdata = document.getElementById("mapWeatherdata");
+  pEl.style.backgroundSize = "cover";
+  pEl.style.backgroundRepeat = "no-repeat";
+  pEl.style.backgroundImage =
+    'url("assets/Images/weather-images/woodBackground.jpg")';
 
   const weathercardCont = document.createElement("div");
   weathercardCont.style.backgroundSize = "cover"; // Adjust this property as needed
@@ -106,11 +117,13 @@ function weatherWidget(
   weathercardCont.style.width = "550px";
   weathercardCont.style.height = "500px";
   weathercardCont.style.position = "relative";
-  weathercardCont.style.margin = "75px";
   weathercardCont.style.borderRadius = "25px";
+  weathercardCont.style.right = "200px";
+  weathercardCont.style.boxShadow = "20px 20px 20px rgba(0, 0, 0, 0.8)";
   weathercardCont.id = "weathercardElement";
+
   //commented out border currently
-  // weathercardCont.style.border = "15px solid black";
+  // weathercardCont.style.border = "8px solid black";
   clearWeatherCard(weathercardCont);
   ifskiesShow(weathercardCont, imgCode, pEl, cityName);
 
@@ -135,6 +148,7 @@ function weatherWidget(
     liEl.style.fontSize = "24px";
     liEl.style.padding = "10px";
     liEl.style.fontFamily = "Raleway, sans-serif";
+    liEl.style.fontWeight = "bold";
 
     ulEl.appendChild(liEl);
   }
@@ -153,35 +167,35 @@ function weatherWidget(
 function ifskiesShow(weathercardCont, imgCode, pEl, cityName) {
   if (imgCode === "13d" || imgCode === "13n") {
     weathercardCont.style.backgroundImage =
-      'url("assets/Images/weather-images/snow.jpg")';
+      'url("assets/Images/weather-images/snowCity.png")';
     pEl.textContent =
       "Currently in " +
       cityName +
       " it is snowing. Grab a drink and stay warm at a nearby brewery. ";
   } else if (imgCode === "11d" || imgCode === "11n") {
     weathercardCont.style.backgroundImage =
-      'url("assets/Images/weather-images/thunderStorm.jpg")';
+      'url("assets/Images/weather-images/Lightning.png")';
     pEl.textContent =
       "Currently in " +
       cityName +
       " there is a thunderstorm. Are you afraid? drink about it at a nearby brewery.";
   } else if (imgCode === "09d" || imgCode === "09n") {
     weathercardCont.style.backgroundImage =
-      'url("assets/Images/weather-images/rainyCity.jpg")';
+      'url("assets/Images/weather-images/cityRain.png")';
     pEl.textContent =
       "Currently in " +
       cityName +
       " it is raining. Stay dry and grab a drink at a nearby brewery. ";
   } else if (imgCode === "10d" || imgCode === "10n") {
     weathercardCont.style.backgroundImage =
-      'url("assets/Images/weather-images/rainyCity.jpg")';
+      'url("assets/Images/weather-images/cityRain.png")';
     pEl.textContent =
       "Currently in " +
       cityName +
       " it is raining. Stay dry and grab a drink at a nearby brewery. ";
   } else if (imgCode === "50d" || imgCode === "50n") {
     weathercardCont.style.backgroundImage =
-      'url("assets/Images/weather-images/notVisible.jpg")';
+      'url("assets/Images/weather-images/lowVis.png")';
     pEl.textContent =
       "Currently in " +
       cityName +
@@ -189,28 +203,28 @@ function ifskiesShow(weathercardCont, imgCode, pEl, cityName) {
     // Visibility low
   } else if (imgCode === "01d" || imgCode === "01n") {
     weathercardCont.style.backgroundImage =
-      'url("assets/Images/weather-images/clearSkies.jpg")';
+      'url("assets/Images/weather-images/skiesClear.png")';
     pEl.textContent =
       "Currently in " +
       cityName +
       " the skies are clear. Enjoy the clear skies and grab a drink at a brewery. ";
   } else if (imgCode === "02d" || imgCode === "02n") {
     weathercardCont.style.backgroundImage =
-      'url("assets/Images/weather-images/aFewclouds.jpg")';
+      'url("assets/Images/weather-images/fewCloudy.png")';
     pEl.textContent =
       "Currently in " +
       cityName +
       " there are a few clouds. It's a perfect day to enjoy the indoors at a brewery. ";
   } else if (imgCode === "03d" || imgCode === "03n") {
     weathercardCont.style.backgroundImage =
-      'url("assets/Images/weather-images/scatteredClouds.jpg")';
+      'url("assets/Images/weather-images/scattered.png")';
     pEl.textContent =
       "Currently in " +
       cityName +
       " there are scattered clouds. It's a perfect day to enjoy the indoors at a brewery. ";
   } else if (imgCode === "04d" || imgCode === "04n") {
     weathercardCont.style.backgroundImage =
-      'url("assets/Images/weather-images/overCast.jpg")';
+      'url("assets/Images/weather-images/overCast.png")';
     pEl.textContent = pEl.textContent =
       "Currently in " +
       cityName +
@@ -244,3 +258,12 @@ function displayImages() {
   images[index - 1].style.display = "block";
   setTimeout(displayImages, 2000);
 }
+
+function dataBackground(mapWeatherdata) {
+  mapWeatherdata.style.backgroundImage =
+    'url("assets/Images/weather-images/tripBackground.jpg")';
+  mapWeatherdata.style.backgroundSize = "cover"; // Adjust this property as needed
+  mapWeatherdata.style.backgroundRepeat = "no-repeat";
+}
+
+dataBackground(mapWeatherdata);
--- assets/js/scriptmap.js
+++ assets/js/scriptmap.js
@@ -1,72 +1,85 @@
-  // This example adds a search box to a map, using the Google Place Autocomplete
-  // feature. People can enter geographical searches. The search box will return a
-  // pick list containing a mix of places and predicted search terms.
-  // This connects the city search btn to the map
+// This example adds a search box to a map, using the Google Place Autocomplete
+// feature. People can enter geographical searches. The search box will return a
+// pick list containing a mix of places and predicted search terms.
+// This connects the city search btn to the map
 
 // var citySearchBtn = document.getElementById("citySearchBtn").addEventListener('click',function() {
 //   document.location.href = '#map';
 // })
 
-  function initAutocomplete() {
-    const map = new google.maps.Map(document.getElementById("map"), {
-      center: { lat: -33.8688, lng: 151.2195 },
-      zoom: 13,
-      mapTypeId: "roadmap",
+function initAutocomplete() {
+  const map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -33.8688, lng: 151.2195 },
+    zoom: 13,
+    mapTypeId: "roadmap",
+  });
+  // Create the search box and link it to the UI element.
+  const input = document.getElementById("pac-input");
+  //styling for input box
+  input.style.width = "400px";
+  input.style.height = "40px";
+  input.style.maxHeight = "40px";
+  input.style.fontSize = "20px";
+
+  input.style.position = "relative";
+  input.style.textAlign = "center";
+  input.style.backgroundSize = "cover";
+  input.style.backgroundRepeat = "no-repeat";
+  input.style.backgroundImage =
+    'url("assets/Images/weather-images/inputBackground.jpg")';
+  input.style.boxShadow = "20px 20px 20px rgba(0, 0, 0, 0.8)";
+  input.style.color = "white";
+
+  const searchBox = new google.maps.places.SearchBox(input);
+  // map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+  // Bias the SearchBox results towards current map's viewport.
+  map.addListener("bounds_changed", () => {
+    searchBox.setBounds(map.getBounds());
+  });
+  let markers = [];
+  // Listen for the event fired when the user selects a prediction and retrieve
+  // more details for that place.
+  searchBox.addListener("places_changed", () => {
+    const places = searchBox.getPlaces();
+    if (places.length == 0) {
+      return;
+    }
+    // Clear out the old markers.
+    markers.forEach((marker) => {
+      marker.setMap(null);
     });
-    // Create the search box and link it to the UI element.
-    const input = document.getElementById("pac-input");
-    const searchBox = new google.maps.places.SearchBox(input);
-    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-    // Bias the SearchBox results towards current map's viewport.
-    map.addListener("bounds_changed", () => {
-      searchBox.setBounds(map.getBounds());
-    });
-    let markers = [];
-    // Listen for the event fired when the user selects a prediction and retrieve
-    // more details for that place.
-    searchBox.addListener("places_changed", () => {
-      const places = searchBox.getPlaces();
-      if (places.length == 0) {
+    markers = [];
+    // For each place, get the icon, name and location.
+    const bounds = new google.maps.LatLngBounds();
+    places.forEach((place) => {
+      if (!place.geometry || !place.geometry.location) {
+        console.log("Returned place contains no geometry");
         return;
       }
-      // Clear out the old markers.
-      markers.forEach((marker) => {
-        marker.setMap(null);
-      });
-      markers = [];
-      // For each place, get the icon, name and location.
-      const bounds = new google.maps.LatLngBounds();
-      places.forEach((place) => {
-        if (!place.geometry || !place.geometry.location) {
-          console.log("Returned place contains no geometry");
-          return;
-        }
-        const icon = {
-          url: place.icon,
-          size: new google.maps.Size(71, 71),
-          origin: new google.maps.Point(0, 0),
-          anchor: new google.maps.Point(17, 34),
-          scaledSize: new google.maps.Size(25, 25),
-        };
-        // Create a marker for each place.
-        markers.push(
-          new google.maps.Marker({
-            map,
-            icon,
-            title: place.name,
-            position: place.geometry.location,
-          }),
-        );
-        if (place.geometry.viewport) {
-          // Only geocodes have viewport.
-          bounds.union(place.geometry.viewport);
-        } else {
-          bounds.extend(place.geometry.location);
-        }
-      });
-      map.fitBounds(bounds);
+      const icon = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25),
+      };
+      // Create a marker for each place.
+      markers.push(
+        new google.maps.Marker({
+          map,
+          icon,
+          title: place.name,
+          position: place.geometry.location,
+        })
+      );
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
     });
-  }
-  window.initAutocomplete = initAutocomplete;
-
-
+    map.fitBounds(bounds);
+  });
+}
+window.initAutocomplete = initAutocomplete;
